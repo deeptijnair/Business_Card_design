7c0d5cac0b54370ad5e7ec701f7b0e47a6f73f80
Deepthi <deeptijnair@gmail.com>
2024-08-28 15:35:36 -0400

    Removed js code

--- card.js
+++
@@ -1,8 +0,0 @@
-const qrcode = new QRCode(document.getElementById('qr-code'), {
-    text: 'http://jindo.dev.naver.com/collie',
-    width: 128,
-    height: 128,
-    colorDark : '#000',
-    colorLight : '#fff',
-    correctLevel : QRCode.CorrectLevel.H
-  });
